4923a7592b418769d149e4e2454248b608813318
MinKown <94045408+MinKown@users.noreply.github.com>
2025-08-27 19:45:40 +0900

    (2025.08.27) 머드 게임 3일차

--- Program/Source.c
+++ Program/Source.c
@@ -12,8 +12,9 @@ struct Player
 	int hp;
 	int attack;
 	int defence;
-	int questPoint;
+	int questPoint;				// 현재 퀘스트 진행도
 	int fightTurn;
+	int successPoint;			// 미션 성공 횟수
 };
 
 struct Monster
@@ -40,7 +41,7 @@ void cls() {
 void makeName(struct  Player* p) {
 	printf("[낡은 여관의 문이 삐걱이며 열린다.]\n");
 	printf("여관주인 : 그쪽은 이름이 어떻게 되나?\n");
-
+	printf("이름 : ");
 	if (fgets(p->name, 49, stdin) != NULL) {
 		p->name[strcspn(p->name, "\n")] = '\0';
 	}
@@ -55,6 +56,7 @@ void choiceJob(struct Player *p) {
 	printf("[달그락거리며 여관주인은 여러 장구류를 가져온다.]\n");
 	printf("여관주인 : 어떤걸 고르시겠습니까?\n");
 	printf("[1. 검 2. 활 3. 지팡이]\n");
+	printf("선택 : ");
 
 	int playerChoice;
 	scanf_s("%d", &playerChoice);
@@ -66,19 +68,19 @@ void choiceJob(struct Player *p) {
 		printf("여관주인 : 오, 튼튼한 검을 집었군.\n");
 		printf("여관주인 : 이제 모험가다운 모습이로군!\n");
 		p->hp = 150; p->attack = 50; p->defence = 50;
-		p->level = 1; p->questPoint = 1; p->fightTurn = 1;
+		p->level = 1; p->questPoint = 1; p->fightTurn = 1; p->successPoint = 0;
 		break;
 	case 2:
 		p->equipment = 2; // 활
 		printf("여관주인 : 조용히, 그러나 멀리 보는 눈을 가진 자네와 잘 어울리는군...\n");
 		p->hp = 100; p->attack = 20; p->defence = 30;
-		p->level = 1; p->questPoint = 1; p->fightTurn = 1;
+		p->level = 1; p->questPoint = 1; p->fightTurn = 1; p->successPoint = 0;
 		break;
 	case 3:
 		p->equipment = 3; // 지팡이
 		printf("여관주인 : 마법을 택했군, 지혜로운 선택일세...\n");
 		p->hp = 100; p->attack = 30; p->defence = 10;
-		p->level = 1; p->questPoint = 1; p->fightTurn = 1;
+		p->level = 1; p->questPoint = 1; p->fightTurn = 1; p->successPoint = 0;
 		break;
 	default:
 		break;
@@ -125,8 +127,7 @@ const char* Talk4[4] = {
 
 int choiceMission() {
 	int choice = 0;
-	printf("1. 숲 가장자리	2. 마을 강가 근처	3. 북쪽 구릉지\n");
-	printf("4. 마을");
+	printf("1. 숲 가장자리	2. 마을 강가 근처	3. 북쪽 구릉지		4. 마을\n");
 	printf("어디로 향하시겠습니까? : ");
 	scanf_s("%d", &choice);
 
@@ -201,24 +202,39 @@ int skill() {
 
 void battle(int stage, struct Monster f[], struct Monster r[], struct Monster h[], int stageTurn, int random) {
 	int damage = 0;
+	int enemyDamage = 0;
 	int trun = stageTurn;
-	
 
 	switch (stage) {
 	case 1:
 		damage = skill();
+		enemyDamage = f[trun].attack;
 
 		if (f[trun].hp <= 0) {
 			trun = 0;
 		}
 		printf("%d 번째 턴\n", trun + 1);
+
+		// player 크리티컬 : 30% 확률로 발동
+		if (random >= 30) {
+			damage += damage;
+			printf("[크리티컬!] ");
+		}
+
 		printf("나의 공격 (%d)\n", damage);
 		f[trun].hp -= damage;
 		
 		if (f[trun].hp > 0) {
-			printf("적의 공격 (%d)\n", f[trun].attack);
-			player.hp -= f[trun].attack;
+			// enemy 크리티컬 : 10% 확률로 발동
+			if (random >= 70) {
+				enemyDamage += enemyDamage;
+				printf("[크리티컬!] ");
+			}
+
+			printf("적의 공격 (%d)\n", enemyDamage);
+			player.hp -= enemyDamage;
 		}
+		printf("\n");
 		break;
 
 	case 2:
@@ -235,7 +251,7 @@ void battle(int stage, struct Monster f[], struct Monster r[], struct Monster h[
 			printf("적의 공격 (%d)\n", r[trun].attack);
 			player.hp -= r[trun].attack;
 		}
-
+		printf("\n");
 		break;
 
 	case 3:
@@ -251,7 +267,7 @@ void battle(int stage, struct Monster f[], struct Monster r[], struct Monster h[
 			printf("적의 공격 (%d)\n", h[trun].attack);
 			player.hp -= h[trun].attack;
 		}
-
+		printf("\n");
 		break;
 	}
 }
@@ -267,6 +283,9 @@ int main() {
 	int i = 0;
 	int stageTurn = 0;					// 전투에서 벌어지는 턴
 	int fightOrder = 0;					// 던전 몬스터의 수
+	int useSmithy = 0;					// 최대 대장간 사용 수
+	int useHotel = 0;					// 최대 여관 사용 수
+	int useStore = 0;					// 최대 상점 사용 수
 
 	bool running = true;
 	bool fighting = true;
@@ -281,37 +300,40 @@ int main() {
 		case 1:		// 스토리 및 임무 받기
 			switch (player.questPoint)
 			{
-				printf("스페이스바를 눌러주세요.\n");
+				
 			case 1:		// 첫 번째 임무
+				printf("스페이스바를 눌러주세요.\n");
 				while (i < 7) {
 					if (GetAsyncKeyState(VK_SPACE) & 0x0001) {
 						printf("%s\n", Talk1[i]);
 						i++;
 					}
 				}
-				state = 2;
+				state = 4;
 				i = 0;
 				break;
 
 			case 2:		// 두 번째 임무
+				printf("스페이스바를 눌러주세요.\n");
 				while (i < 6) {
 					if (GetAsyncKeyState(VK_SPACE) & 0x0001) {
 						printf("%s\n", Talk2[i]);
 						i++;
 					}
 				}
-				state = 2;
+				state = 4;
 				i = 0;
 				break;
 
 			case 3:		// 세 번째 임무
+				printf("스페이스바를 눌러주세요.\n");
 				while (i < 6) {
 					if (GetAsyncKeyState(VK_SPACE) & 0x0001) {
 						printf("%s\n", Talk3[i]);
 						i++;
 					}
 				}
-				state = 2;
+				state = 4;
 				i = 0;
 				break;
 
@@ -322,7 +344,7 @@ int main() {
 						i++;
 					}
 				}
-				state = 2;
+				state = 4;
 				i = 0;
 				break;
 			}
@@ -331,8 +353,8 @@ int main() {
 
 		case 2:		// 임무 장소 선택 및 설명
 			missionChoice = choiceMission();
-			if (missionChoice == !4) {
-				stageInfo(missionChoice, frest, river, hills);
+			if (missionChoice == 1 || missionChoice == 2 || missionChoice == 3) {
+				
 				state = 3;
 			}
 			else state = 4;
@@ -343,6 +365,19 @@ int main() {
 			switch (missionChoice)
 			{
 			case 1:
+				if (player.questPoint < 1) {
+					printf("현재 접근할 수 없어.\n");
+					state = 2;
+					break;
+				}
+				if (player.successPoint >= 1) {
+					printf("이곳에서의 볼 일은 끝났어.\n");
+					state = 2;
+					break;
+				}
+
+				stageInfo(missionChoice, frest, river, hills);
+
 				while (fighting) {
 					printf("나 : %s		체력 : %d	  공격력 : %d		방어력 : %d\n",
 						player.name, player.hp, player.attack, player.defence);
@@ -358,6 +393,7 @@ int main() {
 					if (stageTurn == 2) {		// 모든 적 처치
 						printf("나는 고블린들의 이빨을 뜯어 주머니에 넣었다.\n");
 						player.questPoint++;
+						player.successPoint++;
 						stageTurn = 0;
 						fightOrder = 0;
 						break;
@@ -375,6 +411,20 @@ int main() {
 				break;
 
 			case 2:
+				if (player.questPoint < 2) {
+					printf("현재 접근할 수 없어.\n");
+					state = 2;
+					break;
+				}
+				if (player.successPoint >= 2) {
+					printf("이곳에서의 볼 일은 끝났어.\n");
+					state = 2;
+					break;
+
+				}
+
+				stageInfo(missionChoice, frest, river, hills);
+
 				while (fighting) {
 					printf("나 : %s		체력 : %d	  공격력 : %d		방어력 : %d\n",
 						player.name, player.hp, player.attack, player.defence);
@@ -390,6 +440,7 @@ int main() {
 					if (stageTurn == 3) {		// 모든 적 처치
 						printf("도둑 클리어\n");
 						player.questPoint++;
+						player.successPoint++;
 						stageTurn = 0;
 						fightOrder = 0;
 						break;
@@ -407,6 +458,19 @@ int main() {
 				break;
 
 			case 3:
+				if (player.questPoint < 3) {
+					printf("현재 접근할 수 없어.\n");
+					state = 2;
+					break;
+				}
+				if (player.successPoint >= 3) {
+					printf("이곳에서의 볼 일은 끝났어.\n");
+					state = 2;
+					break;
+				}
+
+				stageInfo(missionChoice, frest, river, hills);
+
 				while (fighting) {
 					printf("나 : %s		체력 : %d	  공격력 : %d		방어력 : %d\n",
 						player.name, player.hp, player.attack, player.defence);
@@ -422,6 +486,7 @@ int main() {
 					if (stageTurn == 4) {		// 모든 적 처치
 						printf("늑대 클리어\n");
 						player.questPoint++;
+						player.successPoint++;
 						stageTurn = 0;
 						fightOrder = 0;
 						break;
@@ -443,26 +508,63 @@ int main() {
 			break;
 
 		case 4:		// 마을
-			printf("마을");
-			printf("1. 여관		2. 대장간		3. 상점\n");
+			printf("마을\n");
+			printf("1. 여관		2. 대장간		3. 상점		4. 임무 수행\n");
 			printf("선택 : ");
 			scanf_s("%d", &viligeChoice);
 
 			switch (viligeChoice)
 			{
 			case 1:		// 여관
-				printf("잠시 휴식을 해야겠어.\n");
-				if (player.equipment == 1) player.hp = 150;
-				if (player.equipment == 2) player.hp = 100;
-				if (player.equipment == 3) player.hp = 100;
+				printf("무엇을 하지 ?\n");
+				printf("1. 휴식		2. 여관주인\n");
+				printf("선택 : ");
+				scanf_s("%d", &hotelChoice);
+				switch (hotelChoice)
+				{
+				case 1:			// 휴식
+					if (useHotel >= 6) {
+						break;
+						state = 4;
+					}
+					printf("잠시 휴식을 해야겠어.\n");
+					printf("현재 %d 번 사용하셨습니다. 총 6번 사용 가능합니다.\n", useHotel + 1);
+					if (player.equipment == 1) player.hp = 150;
+					if (player.equipment == 2) player.hp = 100;
+					if (player.equipment == 3) player.hp = 100;
+
+					useHotel++;
+
+					break;
+				case 2:
+					state = 1;
+					break;
+				default:
+					break;
+				}
 				break;
 			case 2:		// 대장간
+				if (useSmithy >= 6) {
+					break;
+					state = 4;
+				}
 				printf("대장간 사용\n");
+				printf("현재 %d 번 사용하셨습니다. 총 6번 사용 가능합니다.\n", useSmithy + 1);
+				useSmithy++;
+				state = 4;
 				break;
 			case 3:		// 상점
+				if (useStore >= 6) {
+					break;
+					state = 4;
+				}
 				printf("상점 사용\n");
+				printf("현재 %d 번 사용하셨습니다. 총 6번 사용 가능합니다.\n", useStore + 1);
+				useStore++;
+				state = 4;
 				break;
-			default:
+			case 4:
+				state = 2;
 				break;
 			}
 			break;
